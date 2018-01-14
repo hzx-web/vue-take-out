d882b0560bad3dd2104fe64f243a8ab90503fc54
hzx-web <13718547328@163.com>
2018-01-14 14:11:04 +0800

    vue项目初始化

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <keep-alive>
+      <router-view :seller="seller"></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+  import {urlParse} from '@/common/js/util'
+  import Header from '@/components/header/Header'
+  const ERR_OK = 0
+
+  export default {
+    data() {
+      return {
+        seller: {
+          id: (() => {
+            let queryParam = urlParse()
+            return queryParam.id
+          })()
+        }
+      }
+    },
+    created() {
+      let _this = this
+      this.axios.get('/api/seller?id=' + this.seller.id)
+        .then(function (response) {
+          if (response.data.errno === ERR_OK) {
+            _this.seller = Object.assign({}, _this.seller, response.data.data)
+          }
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    },
+    components: {
+      'v-header': Header
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.router-link-active
+          color: rgb(240, 20, 20)
+</style>
---
+++ src/components/ratingselect/RatingSelect.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="ratingselect">
+    <div class="rating-type border-1px">
+      <span @click="select(2,$event)" class="block positive" :class="{'active':selectType===2}">{{desc.all}}<span
+        class="count">{{ratings.length}}</span></span>
+      <span @click="select(0,$event)" class="block positive" :class="{'active':selectType===0}">{{desc.positive}}<span
+        class="count">{{positives.length}}</span></span>
+      <span @click="select(1,$event)" class="block negative" :class="{'active':selectType===1}">{{desc.negative}}<span
+        class="count">{{negatives.length}}</span></span>
+    </div>
+    <div @click="toggleContent" class="switch" :class="{'on':onlyContent}">
+      <span class="icon-check_circle"></span>
+      <span class="text">只看有内容的评价</span>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  const POSITIVE = 0
+  const NEGATIVE = 1
+  const ALL = 2
+
+  export default {
+    props: {
+      ratings: {
+        type: Array,
+        default() {
+          return []
+        }
+      },
+      selectType: {
+        type: Number,
+        default: ALL
+      },
+      onlyContent: {
+        type: Boolean,
+        default: false
+      },
+      desc: {
+        type: Object,
+        default() {
+          return {
+            all: '全部',
+            positive: '满意',
+            negative: '不满意'
+          }
+        }
+      }
+    },
+    data() {
+      return {
+        selectedType: this.selectType,
+        onlyContents: this.onlyContent
+      }
+    },
+    watch: {
+      selectType(val) {
+        this.selectedType = val
+      },
+      selectedType(val) {
+        this.$emit('ratingtypeSelect', val)
+      },
+      onlyContent(val) {
+        this.onlyContents = val
+      },
+      onlyContents(val) {
+        this.$emit('contentToggle', val)
+      }
+    },
+    computed: {
+      positives() {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === POSITIVE
+        })
+      },
+      negatives() {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === NEGATIVE
+        })
+      }
+    },
+    methods: {
+      select(type, event) {
+        if (!event._constructed) {
+          return
+        }
+        this.selectedType = type
+      },
+      toggleContent(event) {
+        if (!event._constructed) {
+          return
+        }
+        this.onlyContents = !this.onlyContents
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
+  .ratingselect
+    .rating-type
+      padding: 18px 0
+      margin: 0 18px
+      border-1px(rgba(7, 17, 27, 0.1))
+      font-size: 0
+      .block
+        display: inline-block
+        padding: 8px 12px
+        margin-right: 8px
+        line-height: 16px
+        border-radius: 1px
+        font-size: 12px
+        color: rgb(77, 85, 93)
+        &.active
+          color: #fff
+        .count
+          margin-left: 2px
+          font-size: 8px
+        &.positive
+          background: rgba(0, 160, 220, 0.2)
+          &.active
+            background: rgb(0, 160, 220)
+        &.negative
+          background: rgba(77, 85, 93, 0.2)
+          &.active
+            background: rgb(77, 85, 93)
+    .switch
+      padding: 12px 18px
+      line-height: 24px
+      border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+      color: rgb(147, 153, 159)
+      font-size: 0
+      &.on
+        .icon-check_circle
+          color: #00c850
+      .icon-check_circle
+        display: inline-block
+        vertical-align: top
+        margin-right: 4px
+        font-size: 24px
+      .text
+        display: inline-block
+        vertical-align: top
+        font-size: 12px
+</style>
+
